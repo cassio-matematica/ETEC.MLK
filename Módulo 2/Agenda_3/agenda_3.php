f7fb90a853d7694b32476999d497a7b4bab87878
Cássio de Albuquerque <86534203+cassio-matematica@users.noreply.github.com>
2022-10-28 19:18:03 -0300

    Systems development

---
+++ Módulo 2/Agenda_3/agenda_3.php
@@ -0,0 +1,22 @@
+<!--Aluno: Cássio de albquerque
+Curso: Técnico em Desenvolvimento de Sistemas
+Disciplina: Desenvolvimento de Sistemas-->
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <title>promoção</title>
+</head>
+
+
+<body>
+
+<h1 class="w3-container w3-red">Promoção de Mês de Aniversário!</h1>
+    
+</body>
+</html>
