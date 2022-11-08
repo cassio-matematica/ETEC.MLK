38712a8ca590433a11722be467aea66521df0568
Cássio de Albuquerque <86534203+cassio-matematica@users.noreply.github.com>
2022-11-08 18:32:52 -0300

    Desenvolvimento de Sistemas

---
+++ Sistemas II/Módulo 2/Agenda_5/agenda_5.php
@@ -0,0 +1,15 @@
+<?php
+$produtosAssociativo = array(
+ array("nome"=> "Processador","valor"=> "900" ),
+ array("nome"=> "Mouse","valor"=> "15" ),
+ array("nome"=> "Teclado","valor"=> "20" ),
+ array("nome"=> "Impressora","valor"=> "500" ),
+ array("nome"=> "Monitor","valor"=> "450" ),
+ array("nome"=> "Placa de Vídeo","valor"=> "1500" ),
+ array("nome"=> "Memória RAM 8G","valor"=> "500" ),
+ array("nome"=> "Placa Mãe","valor"=> "600" ),
+ array("nome"=> "Mouse Pad","valor"=> "25" ),
+ array("nome"=> "SSD","valor"=> "245" ),
+ );
+ echo $produtosAssociativo;
+ ?>
